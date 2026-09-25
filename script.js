4b9c4cf0a26fc480a773fdce3207c7b4f7c241eb
akaletekoffilevis <l.akalete20@gmail.com>
2026-09-25 16:55:16 +0100

    feat: mise a jour de la logique des conditions en utilisant les boucles

--- script.js
+++ script.js
@@ -14,53 +14,63 @@ function LancerDe() {
 
     somme.textContent = " " + result1 + " + " + result2 + " = " + (result1 + result2) + " ";
 
-    switch (result1) {
-        case 1:
-            de1.textContent = DE[0];
-            break;
-        case 2:
-            de1.textContent = DE[1];
-            break;
-        case 3:
-            de1.textContent = DE[2];
-            break;
-        case 4:
-            de1.textContent = DE[3];
-            break;
-        case 5:
-            de1.textContent = DE[4];
-            break;
-        case 6:
-            de1.textContent = DE[5];
-            break;
+    for (let index = 1; index < 7; index++) {
+        if(result1 == index){
+            de1.textContent = DE[index-1];
+        }
 
-        default:
-            de1.textContent = result1;
-            break;
+        if (result2 == index) {
+            de2.textContent = DE[index-1];
+        } 
     }
 
-    switch (result2) {
-        case 1:
-            de2.textContent = DE[0];
-            break;
-        case 2:
-            de2.textContent = DE[1];
-            break;
-        case 3:
-            de2.textContent = DE[2];
-            break;
-        case 4:
-            de2.textContent = DE[3];
-            break;
-        case 5:
-            de2.textContent = DE[4];
-            break;
-        case 6:
-            de2.textContent = DE[5];
-            break;
+    // switch (result1) {
+    //     case 1:
+    //         de1.textContent = DE[0];
+    //         break;
+    //     case 2:
+    //         de1.textContent = DE[1];
+    //         break;
+    //     case 3:
+    //         de1.textContent = DE[2];
+    //         break;
+    //     case 4:
+    //         de1.textContent = DE[3];
+    //         break;
+    //     case 5:
+    //         de1.textContent = DE[4];
+    //         break;
+    //     case 6:
+    //         de1.textContent = DE[5];
+    //         break;
 
-        default:
-            de2.textContent = result2;
-            break;
-    }
+    //     default:
+    //         de1.textContent = result1;
+    //         break;
+    // }
+
+    // switch (result2) {
+    //     case 1:
+    //         de2.textContent = DE[0];
+    //         break;
+    //     case 2:
+    //         de2.textContent = DE[1];
+    //         break;
+    //     case 3:
+    //         de2.textContent = DE[2];
+    //         break;
+    //     case 4:
+    //         de2.textContent = DE[3];
+    //         break;
+    //     case 5:
+    //         de2.textContent = DE[4];
+    //         break;
+    //     case 6:
+    //         de2.textContent = DE[5];
+    //         break;
+
+    //     default:
+    //         de2.textContent = result2;
+    //         break;
+    // }
 }
