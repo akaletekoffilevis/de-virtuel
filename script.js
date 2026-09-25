93eccca1efdb714c8de94e85ae6b70f1b9af7296
akaletekoffilevis <l.akalete20@gmail.com>
2026-09-25 15:56:43 +0100

    feat: ajoute des devrais DE a afficher depuis une liste en utilisant le switch case

--- script.js
+++ script.js
@@ -1,9 +1,40 @@
 const btn = document.getElementById("btnlancer");
 const de = document.getElementById("de");
 
-function LancerDe(){
-    let result = Math.floor(Math.random()*6) + 1;
-    de.textContent = result;
-}
+
 
 btn.addEventListener("click", LancerDe);
+
+
+// ⚀ ⚁ ⚂ ⚃ ⚄ ⚅
+let DE = ["⚀","⚁","⚂","⚃","⚄","⚅"];
+
+
+function LancerDe(){
+    let result = Math.floor(Math.random()*6) + 1;
+    switch (result) {
+        case 1:
+            de.textContent = DE[0];
+            break;
+            case 2:
+            de.textContent = DE[1];
+            break;
+            case 3:
+            de.textContent = DE[2];
+            break;
+            case 4:
+            de.textContent = DE[3];
+            break;
+            case 5:
+            de.textContent = DE[4];
+            break;
+            case 6:
+            de.textContent = DE[5];
+            break;
+    
+        default:
+            de.textContent = result;
+            break;
+    }
+    
+}
